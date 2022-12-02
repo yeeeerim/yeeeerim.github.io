6ecb795dbcf00f76f845921589452c8dd57afdd3
yeeeerim <pppp5097@naver.com>
2022-12-02 23:21:15 +0900

    - Project 페이지 탭 추가

--- src/components/project/ProjectComponent.tsx
+++ src/components/project/ProjectComponent.tsx
@@ -1,20 +1,40 @@
 import styled from "@emotion/styled";
 import { ProjectsSection } from "gatsby-theme-portfolio-minimal/src/sections/Projects";
 import * as classes from "gatsby-theme-portfolio-minimal/src/sections/Projects/";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useLocalDataSource } from "./data";
 import { Animation } from "gatsby-theme-portfolio-minimal/src/components/Animation";
 import { Slider } from "gatsby-theme-portfolio-minimal/src/components/Slider";
 import { Project } from "gatsby-theme-portfolio-minimal/src/components/Project";
 
+export enum TabType {
+  PERSONAL = 0,
+  TEAM = 1,
+}
+
 const ProjectSection = () => {
   const response = useLocalDataSource();
   const data = response.allProjectsJson.sections[0];
 
+  const [selectTab, setSelectTab] = useState<number>(TabType.PERSONAL);
+
   return (
     <ProjectStyled>
-      {/* Personal */}
-      <div className="title">Personal</div>
+      <div className="tab-area">
+        <button
+          className={selectTab === TabType.PERSONAL ? "focus" : ""}
+          onClick={(e) => setSelectTab(TabType.PERSONAL)}
+        >
+          Personal
+        </button>
+        <button
+          className={selectTab === TabType.TEAM ? "focus" : ""}
+          onClick={(e) => setSelectTab(TabType.TEAM)}
+        >
+          Team
+        </button>
+      </div>
+      {/* TODO */}
       <Animation type="fadeIn">
         <Slider
           style={{
@@ -29,7 +49,6 @@ const ProjectSection = () => {
           })}
         </Slider>
       </Animation>
-      <div className="title">Cooperation</div>
     </ProjectStyled>
   );
 };
@@ -44,13 +63,23 @@ const ProjectStyled = styled.div`
   padding: var(--page-padding);
   justify-content: space-between;
 
-  /* common */
-  .title {
-    border: 2px solid white;
-    padding: 4px 15px;
-    border-radius: 20px;
-    display: inline;
-    font-size: 1.5rem;
-    font-weight: 600;
+  .tab-area {
+    button {
+      border: 2px solid white;
+      padding: 4px 15px;
+      border-radius: 15px;
+      display: inline;
+      font-size: 1.5rem;
+      font-weight: 600;
+      cursor: pointer;
+      color: white;
+      background-color: black;
+      margin-right: 10px;
+
+      &.focus {
+        color: black;
+        background-color: white;
+      }
+    }
   }
 `;
