645ba79cc22c6bf62e5f1f2e404e86bb18f2db9c
yerim <pppp5097@naver.com>
2024-05-16 22:26:48 +0900

    - 🚚 : project, diary 페이지 폴더 안으로 이동

--- src/pages/project/index.tsx
+++ src/pages/project/index.tsx
@@ -1,7 +1,7 @@
 import { Seo } from "gatsby-theme-portfolio-minimal";
 import React from "react";
-import ProjectSection from "../components/project/ProjectSection";
-import { Page } from "../components/page";
+import ProjectSection from "../../components/project/ProjectSection";
+import { Page } from "../../components/page";
 
 const ProjectPage = () => {
   return (
